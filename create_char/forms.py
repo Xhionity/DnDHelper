a94137df62364776026e7e35a44709808d708b55
Nikita Kundelev <wpleasegg@gmail.com>
2023-01-26 22:08:55 +0300

    form char

--- create_char/forms.py
+++ create_char/forms.py
@@ -1,1 +1,23 @@
+from django.forms import ModelForm, TextInput, IntegerField
+from .models import Char, Stat
 
+
+class CharForm(ModelForm):
+    class Meta:
+        model = Char
+        fields = ['char_name', 'char_view', 'char_history']
+
+        widgets = {
+            'char_name': TextInput(attrs={
+                'class': '',
+                'placeholder': 'Имя персонажа'
+            }),
+            'char_view': TextInput(attrs={
+                'class': '',
+                'placeholder': 'Внешность персонажа'
+            }),
+            'char_history': TextInput(attrs={
+                'class': '',
+                'placeholder': 'История персонажа'
+            })
+        }
